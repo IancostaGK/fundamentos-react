2f87c93bf893c53d1c31f07fd5a3210590da02e0
Ian Santos <ian.santos@leste.com>
2024-05-15 17:26:49 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <Header author="Ian Santos" content="Lorem Ipsum 1" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post author="Amanda Claro" content="Lorem Ipsum 2" />
+          <Post author="Julinho Claro" content="Lorem Ipsum 3" />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { PencilLine } from "@phosphor-icons/react";
+import styles from "./sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://plus.unsplash.com/premium_photo-1663100722417-6e36673fe0ed?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29kZXxlbnwwfHwwfHx8MA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <img src="https://github.com/iancostagk.png" />
+
+        <strong>Ian Santos</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
