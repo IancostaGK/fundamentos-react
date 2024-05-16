6933d9424f206eb2bb26832323e4b8dd31972052
Ian Santos <ian.santos@leste.com>
2024-05-16 13:59:18 -0300

    feat: post data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,45 @@ import { Post } from "./components/Post";
 import styles from "./App.module.css";
 import { Sidebar } from "./components/Sidebar";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/iancostagk.png",
+      name: "Ian Santos",
+      role: "Software Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+];
+
 function App() {
   return (
     <>
@@ -13,8 +52,16 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post author="Amanda Claro" content="Lorem Ipsum 2" />
-          <Post author="Julinho Claro" content="Lorem Ipsum 3" />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </>
