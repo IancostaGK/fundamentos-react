dc4d9801404a5874e49fc45c81042703a7162b2d
Ian Santos <ian.santos@leste.com>
2024-05-16 10:59:50 -0300

    feat: comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -39,6 +39,14 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe seu comentario" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
